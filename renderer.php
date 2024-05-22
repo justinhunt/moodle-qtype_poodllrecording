24b8bf7ad552dbce4f1b0b4f28c15990f2508726
Justin Hunt <bitwalkerjapan@gmail.com>
2024-05-22 13:35:50 +0900

    fix for issue on M4.4 if grader info is set in question instance settings

--- renderer.php
+++ renderer.php
@@ -72,7 +72,7 @@ class qtype_poodllrecording_renderer extends qtype_renderer {
 
         $question = $qa->get_question();
         return html_writer::nonempty_tag('div', $question->format_text(
-                $question->graderinfo, $question->graderinfo, $qa, 'qtype_poodllrecording',
+                $question->graderinfo, $question->graderinfoformat, $qa, 'qtype_poodllrecording',
             \qtype_poodllrecording\constants::FILEAREA_GRADERINFO, $question->id), array('class' => 'graderinfo'));
     }
 }
--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023051900;
+$plugin->version   = 2024052200;
 $plugin->requires  = 2016052300;//moodle 3.1.0
 $plugin->component = 'qtype_poodllrecording';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.1.09 (Build 2023051900)';
+$plugin->release   = '3.1.10 (Build 2024052200)';
 $plugin->dependencies = array('filter_poodll' => 2016081401);
 
